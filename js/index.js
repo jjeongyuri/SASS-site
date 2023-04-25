310340c7054b1a0bcec18ca3f4e486370166ede7
yuri1004 <jeongyuri1004@nate.com>
2023-04-26 00:10:03 +0900

    0425 집에서 수정

--- js/index.js
+++ js/index.js
@@ -29,11 +29,47 @@ const swiper = new Swiper('.swiper', {
     },
   });
 
-$(function(){
-    함수이름();
+  // box02->gallery
+  $(function(){
+    // li Width 값 변수만들기
+    let liWidth = $('#show li').width();
+    // console.log(liWidth);
+    $('#gallery>.prev').on('click',function(e){
+      $('#show>#all>ul').animate({marginLeft:'-=' + liWidth + 'px'},function(){
+        // 클릭하면 li 첫번째 뒤로 이동+css수정
+        $('#show>#all>ul>li:first-child').appendTo('#show>#all>ul');
+        $('#show>#all>ul').css('margin-left','-' + liWidth + 'px');
+      });
+    });
+    $('#gallery>.next').on('click',function(e){
+      $('#show>#all>ul').animate({marginLeft:'+=' + liWidth + 'px'});
+      $('#show>#all>ul>li:last-child').prependTo('#show>#all>ul');
+      $('#show>#all>ul').css('margin-left','+' + liWidth + 'px');
+    });
+  });
+
+  $(function(){
+    $('#men>ul>li:first-child>a').addClass('.selected');
+    $('#men>ul>li>a').on('click',function(e){
+      let aName = $(this).attr('href');
+      let Wide = $('.see').width();
+      // console.log(Wide);
+      $(aName).show();
+      $('#men>ul>li>a').removeClass('selected');
+      $(this).addClass('selected');
+    })
+    })
 
 
-})
 
 
-function 함수이름(){}
+
+
+// $(function(){
+//     함수이름();
+
+
+// })
+
+
+// function 함수이름(){}
